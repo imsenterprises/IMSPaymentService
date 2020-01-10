8cbde44e99a55f92c3cb73ba0523c3328cacb8f1
Adam Shumann <AShumann@harriscomputer.com>
2020-01-10 09:12:54 -0600

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = IMSPaymentService.Helper.GetClient(@"https://countyprocessing.com/paymentAPI/service/", "testing", "testing");
+            var resp = client.getPaymentForm("b7243b35-b07b-404f-a90c-ec880cb79f32", 123.45f, "");
+            Console.WriteLine(resp);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ IMSPaymentService/Helper.cs
@@ -0,0 +1,24 @@
+﻿using IMSPaymentService.PaymentAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMSPaymentService
+{
+    public class Helper
+    {
+        public static PaymentServicePortTypeClient GetClient(string url, string username, string password)
+        {
+            var binding = new BasicHttpBinding(BasicHttpSecurityMode.Transport);
+            binding.Security.Transport.ClientCredentialType = HttpClientCredentialType.Basic;
+            var address = new EndpointAddress(url);
+            var client = new PaymentServicePortTypeClient(binding, address);
+            client.ClientCredentials.UserName.UserName = username;
+            client.ClientCredentials.UserName.Password = password;
+            return client;
+        }
+    }
+}
